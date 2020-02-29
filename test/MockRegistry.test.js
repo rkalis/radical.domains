8bca27dd55707f4547f7bd9c7b9119bfa556009b
Richard Brady <rnbrady@gmail.com>
2020-02-29 15:35:40 +0000

    mockregistry tests

--- test/MockRegistry.test.js
+++ test/MockRegistry.test.js
@@ -1,11 +1,32 @@
 const MockRegistry = artifacts.require('MockRegistry');
 const { assert } = require("chai");
+const truffleAssert = require('truffle-assertions');
 
 contract('MockRegistry', (accounts) => {
-  describe("Backing functionality", () => {
+
+  describe("Deploying", () => {
     it('should deploy', async () => {
       const registry = await MockRegistry.new({ from: accounts[0] });
       assert.isNotNull(registry);
     });
+  });
+
+  describe("Registering names", () => {
+
+    it('should allow a name to be registered', async () => {
+      const mockRegistry = await MockRegistry.deployed();
+      const nameToRegister = web3.utils.stringToHex("richard.eth");
+      const tx = await mockRegistry.register(nameToRegister, {from: accounts[1]});
+      truffleAssert.eventEmitted(tx, "NewRegistration", {registrant: accounts[1], name: nameToRegister});
+    });
+
+    it('should not allow a name to be registered a second time', async () => {
+      const mockRegistry = await MockRegistry.deployed();
+      const nameToRegister = web3.utils.stringToHex("richard.eth");
+      truffleAssert.reverts(mockRegistry.register(nameToRegister, {from: accounts[1]})); 
+    });
+
+    // it supports transferring to another address
+
   });
 });
