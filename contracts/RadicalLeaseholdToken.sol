935a21c2d69466ad741313b8b175c94ad15e078c
Rosco Kalis <roscokalis@gmail.com>
2020-02-29 16:33:04 +0000

    Payment
    Manager:
    - Leaseholder can deposit (prepay) rent
    - Leaseholder can withdraw remaining rent (if its not owed)
    - Freeholder can collect rent based on time passed since last collection
    - If (at collection) not enough rent is deposited, the token is repossessed
    
    Leasehold
    - When setting price, old rents are collected
    - When leasehold is bought, old rents are settled, and buyer deposits
    new rent

--- contracts/RadicalLeaseholdToken.sol
+++ contracts/RadicalLeaseholdToken.sol
@@ -2,13 +2,18 @@ pragma solidity ^0.6.0;
 
 import "openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "./RadicalManager.sol";
 
 contract RadicalLeaseholdToken is ERC721, Ownable {
-    mapping (uint256 => uint256) private _tokenRent;
+    using SafeMath for uint256;
+
+    mapping (uint256 => uint256) private _tokenRate;
     mapping (uint256 => uint256) private _tokenPrice;
 
     event PriceChanged(address indexed owner, uint256 indexed tokenId, uint256 oldPrice, uint256 newPrice);
     event Sold(address indexed from, address indexed to, uint256 indexed tokenId, uint256 price);
+    event Repossessed(address indexed from, address indexed to, uint256 indexed tokenId);
 
     modifier onlyHolder(uint256 tokenId) {
         require(msg.sender == ownerOf(tokenId), "RadicalLeaseholdToken: only token holder can perform this action");
@@ -16,7 +21,7 @@ contract RadicalLeaseholdToken is ERC721, Ownable {
     }
 
     modifier notHolder(uint256 tokenId) {
-        require(msg.sender == ownerOf(tokenId), "RadicalLeaseholdToken: the token holder cannot perform this action");
+        require(msg.sender != ownerOf(tokenId), "RadicalLeaseholdToken: the token holder cannot perform this action");
         _;
     }
 
@@ -29,35 +34,66 @@ contract RadicalLeaseholdToken is ERC721, Ownable {
         return _tokenPrice[tokenId];
     }
 
+    function rateOf(uint256 tokenId) public view onlyExistingToken(tokenId) returns (uint256) {
+        return _tokenRate[tokenId];
+    }
+
+    // Calculate rent per year with the rate and price
+    function rentOf(uint256 tokenId) public view onlyExistingToken(tokenId) returns (uint256) {
+        return priceOf(tokenId).mul(1000).div(rateOf(tokenId));
+    }
+
+    // Mint new token with initial price and rate
+    function mint(address to, uint256 tokenId, uint256 initialPrice, uint256 rate) public onlyOwner {
+        _mint(to, tokenId);
+        _tokenPrice[tokenId] = initialPrice;
+        _tokenRate[tokenId] = rate;
+    }
+
+    // Set a new price for the token
+    // Causes existing deposited rents to be collected by the freeholder based on the old price
+    // CAUTION: If the rent balance is too low this will cause repossession
     function setPriceOf(uint256 tokenId, uint256 newPrice) public onlyHolder(tokenId) onlyExistingToken(tokenId) {
+        // Settle using old price first
+        RadicalManager(owner()).collectRent(tokenId);
+
+        // Set new price
         uint256 oldPrice = _tokenPrice[tokenId];
         _tokenPrice[tokenId] = newPrice;
-
-        // TODO: Refund old prepaid rent
-        // TODO: Prepay new rent
-
         emit PriceChanged(ownerOf(tokenId), tokenId, oldPrice, newPrice);
     }
 
-    function rentOf(uint256 tokenId) public view onlyExistingToken(tokenId) returns (uint256) {
-        return _tokenRent[tokenId];
-    }
-
-    function mint(address to, uint256 tokenId, uint256 initialPrice, uint256 rent) public onlyOwner {
-        _mint(to, tokenId);
-        _tokenRent[tokenId] = rent;
-        _tokenPrice[tokenId] = initialPrice;
-    }
-
+    // Buys the token from the current owner at the set price
+    // Causes deposited rents to be collected by the freeholder
+    // (and leftover be sent to the old owner)based on the old price
+    // Extra ether sent to this function is used as rent deposit
+    // CAUTION: If the rent balance is too low this will cause repossession
+    // CAUTION: Some additional ETH needs to be sent to this function as rent,
+    // if this is not done, the token is at risk of repossession.
     function buy(uint256 tokenId) public payable notHolder(tokenId) onlyExistingToken(tokenId) {
         uint256 price = priceOf(tokenId);
-        require(msg.value == price, "RadicalLeaseholdToken: did not provide right amount of ETH for purchase");
+        require(msg.value >= price, "RadicalLeaseholdToken: did not provide right amount of ETH for purchase");
+
+        // Settle existing rent payments
+        RadicalManager(owner()).collectRent(tokenId);
+        RadicalManager(owner()).withdrawRent(tokenId, 2 ** 256 - 1);
+
+        // Trade token for price
         address from = ownerOf(tokenId);
         _transferFrom(from, msg.sender, tokenId);
+        payable(from).transfer(price);
 
-        // TODO: Refund prepaid rent to old owner
-        // TODO: Prepay new rent from new owner
+        // Prepay rent
+        uint256 leftover = msg.value - price;
+        RadicalManager(owner()).depositRent.value(leftover)(tokenId);
 
         emit Sold(from, msg.sender, tokenId, price);
+    }
+
+    // Repossess the token to a new owner. Can only be called by the manager.
+    function repossess(address to, uint256 tokenId) public payable onlyOwner {
+        address from = ownerOf(tokenId);
+        _transferFrom(from, to, tokenId);
+        emit Repossessed(from, to, tokenId);
     }
 }
--- contracts/RadicalManager.sol
+++ contracts/RadicalManager.sol
@@ -1,46 +1,118 @@
 pragma solidity ^0.6.0;
 
 import "openzeppelin-solidity/contracts/token/ERC721/IERC721Receiver.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "./RadicalFreeholdToken.sol";
 import "./RadicalLeaseholdToken.sol";
 
 contract RadicalManager is IERC721Receiver {
+    using SafeMath for uint256;
+
     RadicalLeaseholdToken leasehold;
     RadicalFreeholdToken freehold;
     bytes4 constant ERC721_RECEIVED = 0xf0b9e5ba;
+
+    mapping (uint256 => uint256) _depositedRent;
+    mapping (uint256 => uint256) _lastRentSettlement;
+
+    // TODO
+    // event RentDeposited
+    // event RentWithdrawn
+    // event RentCollected
+
+    modifier onlyLeaseholderOrContract(uint256 tokenId) {
+        require(
+            msg.sender == leasehold.ownerOf(tokenId) || msg.sender == address(leasehold),
+            "RadicalManager: action can only be performed by leaseholder or contract"
+        );
+        _;
+    }
+
+    modifier onlyFreeholder(uint256 tokenId) {
+        require(msg.sender == freehold.ownerOf(tokenId), "RadicalManager: action can only be performed by freeholder");
+        _;
+    }
 
     constructor() public {
         leasehold = new RadicalLeaseholdToken();
         freehold = new RadicalFreeholdToken();
     }
 
-    function _parseBytes(bytes memory b) private returns (uint256, uint256) {
+    // Parse data sent to onReceivedERC721
+    function _parseBytes(bytes memory b) private pure returns (uint256, uint256) {
         uint256 price;
         uint256 rent;
-        for(uint i=0;i<64;i++){
-            if(i < 32){
-                price = price + uint8(b[i])*(2**(8*(32-(i+1)))); // 1, 2, 3
-            }
-            else{
-                rent = rent + uint8(b[i])*(2**(8*(32-(i-31)))); // 33, 34
+        for (uint i = 0; i < 64; i++) {
+            if(i < 32) {
+                price = price.add(uint8(b[i]) * (2 ** (8 * (32 - (i + 1))))); // 1, 2, 3
+            } else {
+                rent = rent.add(uint8(b[i]) * (2 ** (8 * (32 - (i - 31))))); // 33, 34
             }
         }
         return (price, rent);
     }
 
+    // When an ERC721 token is sent to the manager, a leasehold and freehold token
+    // are minted using the passed parameters.
     function onERC721Received(
         address operator,
         address from,
         uint256 tokenId,
         bytes memory data
     ) public virtual override returns (bytes4) {
+        // TODO: Check that only ENS tokens can be used
         (uint256 price, uint256 rent) = _parseBytes(data);
         leasehold.mint(from, tokenId, price, rent);
         freehold.mint(from, tokenId);
         return ERC721_RECEIVED;
     }
 
-    // Probably use Sablier for this
-    // payRent(tokenId) payable onlyLeaseHolder
-    // collectRent(tokenId) onlyFreeHolder
+    // Deposit rent as prepayment
+    function depositRent(uint256 tokenId) public payable onlyLeaseholderOrContract(tokenId) {
+        _depositedRent[tokenId] = _depositedRent[tokenId].add(msg.value);
+    }
+
+    // Withdraw an amount of prepaid rent
+    // Can only be withdrawn if it is not claimable by the freeholder
+    // CAUTION: If no/little rent is left, the leasehold is at risk of liquidation
+    function withdrawRent(uint256 tokenId, uint256 amount) public onlyLeaseholderOrContract(tokenId) {
+        uint256 rent = calculateCurrentRent(tokenId);
+        uint256 depositedRent = _depositedRent[tokenId];
+        require(depositedRent >= rent, "RadicalManager: not enough rent deposited");
+
+        uint256 leftover = _depositedRent[tokenId].sub(rent);
+        uint256 withdrawAmount = leftover < amount ? leftover : amount;
+
+        address leaseholder = leasehold.ownerOf(tokenId);
+        payable(leaseholder).transfer(withdrawAmount);
+    }
+
+    // Collect currently owed rent.
+    // Calculates the amount of rent that is due, and withdraws this.
+    // Updates the last settlement variable.
+    // If the deposited rent can not cover the rent owed, the leasehold token is
+    // repossessed to the freeholder.
+    function collectRent(uint256 tokenId) public {
+        uint256 rent = calculateCurrentRent(tokenId);
+        _lastRentSettlement[tokenId] = block.timestamp;
+        address freeholder = freehold.ownerOf(tokenId);
+
+        // If the leaseholder can't pay their rent, their property is repossed
+        if (_depositedRent[tokenId] < rent) {
+            rent = _depositedRent[tokenId];
+            leasehold.repossess(freeholder, tokenId);
+        }
+
+        // Transfer rent to freeholder
+        payable(freeholder).transfer(rent);
+    }
+
+    function calculateCurrentRent(uint256 tokenId) private view returns (uint256) {
+        uint256 lastSettlement = _lastRentSettlement[tokenId];
+        uint256 timePassed = block.timestamp - lastSettlement;
+        uint256 rentPerYear = leasehold.rentOf(tokenId);
+        uint256 rent = rentPerYear.div(52 weeks).mul(timePassed); // Not entirely accurate due to leap years
+
+        return rent;
+    }
 }
