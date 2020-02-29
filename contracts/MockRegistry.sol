ec1966dffc92fa6a052b5985a9471994fa6f54f6
Richard Brady <rnbrady@gmail.com>
2020-02-29 09:59:10 +0000

    mockregistry

--- contracts/MockRegistry.sol
+++ contracts/MockRegistry.sol
@@ -1,7 +1,23 @@
-pragma solidity ^0.5.1;
+pragma solidity ^0.6.3;
 
 import "openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
 
 contract MockRegistry is ERC721 {
+
+    struct Record {
+        address owner;
+        address resolver;
+        uint64 ttl;
+    }
+
+    mapping (string => Record) records;
+
+    function register(string name, address owner) {
+        records[name].owner = msg.sender;
+    }
+
+
+
+
 
 }
