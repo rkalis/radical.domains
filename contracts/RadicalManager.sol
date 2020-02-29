190b6d5f75a57af53e89b572107b32a041006645
Rosco Kalis <roscokalis@gmail.com>
2020-02-29 12:09:46 +0000

    Some solidity fixes

--- contracts/RadicalManager.sol
+++ contracts/RadicalManager.sol
@@ -1,20 +1,25 @@
 pragma solidity ^0.6.0;
 
-import "openzeppelin-solidity/contracts/token/ERC721/ERC721Receiver.sol";
-import "./RadicalFreeholdToken";
-import "./RadicalLeaseholdToken";
+import "openzeppelin-solidity/contracts/token/ERC721/IERC721Receiver.sol";
+import "./RadicalFreeholdToken.sol";
+import "./RadicalLeaseholdToken.sol";
 
-contract RadicalManager is ERC721Receiver {
+contract RadicalManager is IERC721Receiver {
     RadicalLeaseholdToken leasehold;
     RadicalFreeholdToken freehold;
     bytes4 constant ERC721_RECEIVED = 0xf0b9e5ba;
 
-    constructor(address _leasehold, address _freehold) {
+    constructor(address _leasehold, address _freehold) public {
         leasehold = RadicalLeaseholdToken(_leasehold);
         freehold = RadicalFreeholdToken(_freehold);
     }
 
-    function onERC721Received(address _from, uint256 _tokenId, bytes _data) public returns(bytes4) {
+    function onERC721Received(
+        address operator,
+        address from,
+        uint256 tokenId,
+        bytes memory data
+    ) public virtual override returns (bytes4) {
         return ERC721_RECEIVED;
     }
 
